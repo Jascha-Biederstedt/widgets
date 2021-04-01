fc4df1a622b3278e6110a369d0aa137e37303cb5
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-04-01 11:56:57 +0200

    Build search ui an implement request to wikipedia

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Accordion from './components/Accordion';
+import Search from './components/Search';
 
 const items = [
   {
@@ -17,7 +18,12 @@ const items = [
 ];
 
 const App = () => {
-  return <Accordion items={items} />;
+  return (
+    <div>
+      {/* <Accordion items={items} /> */}
+      <Search />
+    </div>
+  );
 };
 
 export default App;
