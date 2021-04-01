0038f9016835df24b6c0dc178a4a3d82ee7c4f45
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-04-01 12:44:37 +0200

    Render list of search results and link to wikipedia article

--- src/components/Search.js
+++ src/components/Search.js
@@ -27,6 +27,25 @@ const Search = () => {
     }
   }, [term]);
 
+  const renderedResults = results.map(result => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <div className="ui form">
@@ -39,6 +58,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
